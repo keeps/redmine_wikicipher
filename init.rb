42d46c7665767fda79ae185cf40f1b54cb49285d
Sébastien Leroux <sleroux@keep.pt>
2013-04-04 11:08:44 +0100

    Updated version to 0.0.5

--- init.rb
+++ init.rb
@@ -14,7 +14,7 @@ Redmine::Plugin.register :redmine_wikicipher do
   author 'Sébastien Leroux'
   author_url 'mailto:sleroux@keep.pt'
   description 'This plugin adds the ability to encrypt section of text'
-  version '0.0.4'
+  version '0.0.5'
   url 'https://github.com/keeps/redmine_wikicipher'
 end
 
