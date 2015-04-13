cb049119e6e3fed6f13413672ac50d23e608d68d
sleroux-keep <sleroux@keep.pt>
2015-04-13 12:07:22 +0100

    Wikicipher viewer moved from app/views/issues to app/views/wiki

--- lib/redmine_wikicipher/macros.rb
+++ lib/redmine_wikicipher/macros.rb
@@ -3,24 +3,24 @@ module RedmineWikicipher
     Redmine::WikiFormatting::Macros.register do
       desc "Coded start tag"
       macro :coded_start do |obj, args|
-        render :partial => "issues/redmine_wikicipher_start", :layout => false,
+        render :partial => "wiki/redmine_wikicipher_start", :layout => false,
                :locals => { :title => args.first }
       end
 
       desc "Coded stop tag"
       macro :coded_stop do |obj, args|
-        render :partial => "issues/redmine_wikicipher_end", :layout => false
+        render :partial => "wiki/redmine_wikicipher_end", :layout => false
       end
 
 
       desc "Decoded start tag"
       macro :decoded_start do |obj, args|
-        render :partial => "issues/redmine_wikicipher2_start", :layout => false,
+        render :partial => "wiki/redmine_wikicipher2_start", :layout => false,
                :locals => { :title => args.first }
       end
       desc "Decoded stop tag"
       macro :decoded_stop do |obj, args|
-        render :partial => "issues/redmine_wikicipher2_end", :layout => false
+        render :partial => "wiki/redmine_wikicipher2_end", :layout => false
       end
     end
   end
