b8b2e59b6df0970819b466b7c789497f803c1c98
Paul van Tilburg <paul.van.tilburg@leftclick.eu>
2016-08-10 11:29:45 +0200

    Always show the cipher button

--- assets/javascripts/jstoolbar/wikicipher.js
+++ assets/javascripts/jstoolbar/wikicipher.js
@@ -17,18 +17,4 @@ window.onload=function(){
 	if (warn.length>0){
 		document.getElementsByClassName('jstb_wikicipher')[0].hide();
 	}
-	var pathName = window.location.pathname;
-	if(
-		(pathName.indexOf(wiki)>=0 && pathName.indexOf(wikiEdit, pathName.length - wikiEdit.length) !== -1)
-		||
-		(pathName.indexOf(wiki, pathName.length - wiki.length) !== -1)
-	){
-		// nothing to do...
-	}else{
-		//hide wikicipher toolbar buttons
-		var elems = document.getElementsByClassName('jstb_wikicipher');
-		for(var i = 0; i != elems.length; ++i){
-			elems[i].style.visibility = "hidden";
-		}
-	}
 };
