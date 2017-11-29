61842cc062337fd8cd57578ea12e96ddd0476674
Tiago Oliveira <tiago.m.oliveira@me.com>
2017-11-29 11:29:37 +0000

    jstoolbar restored

---
+++ assets/javascripts/jstoolbar/lang/wikicipher-en.js
@@ -0,0 +1,4 @@
+jsToolBar.strings['Encrypt tag'] = 'Add encrypt tags';
+jsToolBar.strings['Lock'] = 'Encrypt';
+jsToolBar.strings['Unlock'] = 'Decrypt';
+jsToolBar.strings['Wikicipher tag'] = 'Cipher';
---
+++ assets/javascripts/jstoolbar/lang/wikicipher-pt.js
@@ -0,0 +1,4 @@
+jsToolBar.strings['Encrypt tag'] = 'Adicionar tags de encriptação';
+jsToolBar.strings['Lock'] = 'Encriptar';
+jsToolBar.strings['Unlock'] = 'Decriptar';
+jsToolBar.strings['Wikicipher tag'] = 'Encriptar';
